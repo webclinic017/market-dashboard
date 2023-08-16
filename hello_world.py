6ce54015b6372b0b0a29c13fb7b9f5e17d6cc1b2
Poppingfresh <poppingfresh6699@gmail.com>
2023-08-16 12:51:02 -0400

    Rework RRG plots
    Lookback made to 1 year and try to force the default matplotlib for RRG plots.

--- hello_world.py
+++ hello_world.py
@@ -33,7 +33,7 @@ import correlation
 import realizedvol
 import dressing
 import volCone
-import pairTrader
+# import pairTrader
 import futuresChart
 
 class ListConverter(BaseConverter):
@@ -151,11 +151,11 @@ def realized_vol_term_json(ticker):
 @app.route('/rrg')
 @app.route('/rrg/<rrg_set>')
 def rrg(rrg_set):
-    start_date = '2022-01-01'
+    start_date = (datetime.today()+timedelta(days=-365)).strftime('%Y-%m-%d')
     end_date = datetime.today().strftime('%Y-%m-%d')
     benchmark = 'SPY'
     num_hist = -10
-
+    plt.style.use('default')
     # Define tickers and benchmark
     if rrg_set == '1' or not rrg_set:
         tickers = ['XLP', 'XLE', 'XLF', 'XLRE', 'XLV', 'XLC','XLB','XLI','XLU','XLY','XLK', 'XBI', 'XRT', 'QQQ', 'XHB', 'SMH']
@@ -188,31 +188,31 @@ def IVCone(ticker = 'SPY', start_date = (datetime.today()+timedelta(days=-365)).
     plt.clf()
     return send_file(buf, mimetype='image/png')
     
-@app.route('/pairsMaster')
-@app.route('/pairsMaster/<ticker_1>')
-@app.route('/pairsMaster/<ticker_1>/<ticker_2>')
-def pairsMaster(ticker_1, ticker_2):
-    if ticker_1 == ticker_2:
-        return
-    if ticker_1 == 'scan' and ticker_2 == 'active':
-        #Scans Nasdaq 100 stocks for potential pairs that meet certain criteria
-        scan_results = pairTrader.scanner()
-        plt.table(scan_results)
-        plt.title('Scan Results')
-        plt.axis('off')
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
-        plt.clf()
-        return send_file(buf, mimetype='image/png')
+# @app.route('/pairsMaster')
+# @app.route('/pairsMaster/<ticker_1>')
+# @app.route('/pairsMaster/<ticker_1>/<ticker_2>')
+# def pairsMaster(ticker_1, ticker_2):
+#     if ticker_1 == ticker_2:
+#         return
+#     if ticker_1 == 'scan' and ticker_2 == 'active':
+#         #Scans Nasdaq 100 stocks for potential pairs that meet certain criteria
+#         scan_results = pairTrader.scanner()
+#         plt.table(scan_results)
+#         plt.title('Scan Results')
+#         plt.axis('off')
+#         buf = io.BytesIO()
+#         plt.savefig(buf, format='png')
+#         buf.seek(0)
+#         plt.clf()
+#         return send_file(buf, mimetype='image/png')
     
-    spread_l, beta_l, r_sq_l, pval_l, adf_stats_l, zscores_l = pairTrader.spreadMain(ticker_1, ticker_2, -180)
-    spread_s, beta_s, r_sq_s, pval_s, adf_stats_s, zscores_s = pairTrader.spreadMain(ticker_1, ticker_2, -60)
-    buf = pairTrader.plot_pairs(spread_l, beta_l, r_sq_l, pval_l, adf_stats_l, zscores_l,
-                                spread_s, beta_s, r_sq_s, pval_s, adf_stats_s, zscores_s,
-                                ticker_1, ticker_2)
-    plt.clf()
-    return send_file(buf, mimetype='image/png')
+#     spread_l, beta_l, r_sq_l, pval_l, adf_stats_l, zscores_l = pairTrader.spreadMain(ticker_1, ticker_2, -180)
+#     spread_s, beta_s, r_sq_s, pval_s, adf_stats_s, zscores_s = pairTrader.spreadMain(ticker_1, ticker_2, -60)
+#     buf = pairTrader.plot_pairs(spread_l, beta_l, r_sq_l, pval_l, adf_stats_l, zscores_l,
+#                                 spread_s, beta_s, r_sq_s, pval_s, adf_stats_s, zscores_s,
+#                                 ticker_1, ticker_2)
+#     plt.clf()
+#     return send_file(buf, mimetype='image/png')
 
 @app.route('/vixBasis')
 def vix_basis():
